2450e4e0545d0673ef9ac63f490a180292312fb0
Akeltroll <akeltroll@gmail.com>
2025-10-31 14:06:12 +0100

    fix migration create_books

--- database/migrations/2025_10_24_141621_create_books_table.php
+++ database/migrations/2025_10_24_141621_create_books_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->enum('type', ['manga', 'webtoon', 'autre'])->default('manga');
+            $table->string('web_link');
+            $table->text('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->boolean('is_featured')->default(false);
+            
+            // Clé étrangère vers users (pour lier chaque livre à un utilisateur)
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            
             $table->timestamps();
         });
     }
